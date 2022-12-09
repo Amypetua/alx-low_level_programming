795a8e9c387e256082606ab5a89279c911fb96ca
Amypetua <anachusichiamaka30@gmail.com>
2022-12-09 06:34:06 -0800

    Sizes

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - Entry poit
+ * main - Entry point
  *
  * Return: 0
  */
- int main(void)
- {
-	 puts("\"Programming is like building a multilingual puzzle");
-	 return (0);
+int main(void)
+{
+       	puts("Programming is like building a multilingual puzzle\n");
+       	return (0);
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,9 +14,9 @@ int main(void)
 	float f;
 
 
-	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
-	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
-	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
-	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+	printf("Size of an int: %i byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of a long int: %i byte(s)\n", (unsigned long)sizeof(b));
+	printf("Size of a long long int: %i byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of a float: %i byte(s)\n", (unsigned long)sizeof(f));
 	return (0);
 }
