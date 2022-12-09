6cf681a88ead7534874d50f13561c952cc3bf13f
Amypetua <anachusichiamaka30@gmail.com>
2022-12-08 17:58:22 -0800

    Hello put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,12 @@
-#include <stdio.h>
-int main(void) {puts("\"Programming is like building a multilingual puzzle); return (0);}
+#include <stdio>
+
+/**
+ * main -Entry point
+ *
+ * Return: Always 0
+ */
+ int main(void)
+ {
+	 puts("\"Programming is like building a multilingual puzzle")
+		 return (0);
+ }
