fb662a558733413ae8115cddbfb225fd207edf6c
Amypetua <anachusichiamaka30@gmail.com>
2022-12-09 08:15:08 -0800

    Size is not grandeur

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,17 +6,14 @@
  */
 int main(void)
 {
-	int a;
-	long int b;
-	long long int c;
-	char d;
-	float f;
-
-
-	print("Size of a char: %c byte(s)\n", (unsigned long)sizeof(d));
-	printf("Size of an int: %i byte(s)\n", (unsigned long)sizeof(a));
-	printf("Size of a long int: %li byte(s)\n", (unsigned long)sizeof(b));
-	printf("Size of a long long int: %lli byte(s)\n", (unsigned long)sizeof(c));
-	printf("Size of a float: %f byte(s)\n", (unsigned long)sizeof(f));
+	printf("Size of a char: %c byte(s)\n", sizeof(char));
+	printf("Size of an int: %i byte(s)\n", sizeof(int));
+	printf("Size of a long int: %li byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %lli byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %f byte(s)\n", sizeof(float));
+	printf("Size of a double: %g byte(s)\n", sizeof(double));
+	printf("Size of a long double: %lg byte(s)\n", sizeof(long double));
+	printf("Size of an unsigned int: %u byte(s)\n", sizeof(unsigned int));
+	printf("Size of an unsigned long int: %lu byte(s)\n", sizeof(unsigned long int));
 	return (0);
-
+}
