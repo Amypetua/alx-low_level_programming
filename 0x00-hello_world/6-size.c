98c045745719e526bc51154f5888bf9387cc4c9b
Amypetua <anachusichiamaka30@gmail.com>
2022-12-09 06:58:55 -0800

    Sizes of var

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,6 +17,7 @@ int main(void)
 	printf("Size of an int: %i byte(s)\n", (unsigned long)sizeof(a));
 	printf("Size of a long int: %i byte(s)\n", (unsigned long)sizeof(b));
 	printf("Size of a long long int: %i byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of a char: %i byte(s)n", (unsigned char)sizeof(d))
 	printf("Size of a float: %i byte(s)\n", (unsigned long)sizeof(f));
 	return (0);
 }
