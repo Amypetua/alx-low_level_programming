e8aed1ddb66848c1305a767708bf5a11a42bac51
Amypetua <anachusichiamaka30@gmail.com>
2022-12-11 15:58:41 -0800

    Alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio>
+/**
+ * main - print alphabet in lower case
+ * Return: Always 0
+ */
+int main(void)
+{
+	char lc;
+
+	for (lc 'a'; lc <= 'z'; lc++)
+	{
+		putchar(lc);
+	}
+	putchar('\n');
+	return (0);
+}
