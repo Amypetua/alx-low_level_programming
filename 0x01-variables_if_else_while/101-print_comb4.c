c864c7e521e6276e07e7f05cc6e9fc882acc921f
Amypetua <anachusichiamaka30@gmail.com>
2022-12-12 20:30:46 -0800

    The success combination in business

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,14 +10,14 @@
 int main(void)
 {
 	int ones = '0';
-	int tens = '0'
-	int hundrad = '0';
+	int tens = '0';
+	int hundreds = '0';
 
 	for (hundreds = '0'; hundreds <= '9'; hundreds++)
 	{
 		for (tens = '0'; tens <= '9'; tens++)
 		{
-			for (ones = '0'; tens = '9'; ones++)
+			for (ones = '0'; ones = '9'; ones++)
 				if (!((ones == tens) || (tens == hundreds) || (tens > ones) || (hundreds > tens)))/*eliminate repetition*/
 				{
 					putchar(hundreds);
@@ -31,7 +31,7 @@ int main(void)
 				}
 		}
 	}
-	puchar('\n');
+	putchar('\n');
 
 	return (0);
 }
