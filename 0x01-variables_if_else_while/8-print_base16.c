a5243c15e4806beefa08be324d98d6bea08ea5dd
Amypetua <anachusichiamaka30@gmail.com>
2022-12-12 18:17:27 -0800

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int d;
+	char low;
+
+	for (d = '0'; d <= '9'; d++)
+		putchar (d);
+	for (low = 'a'; low <= 'f'; low++)
+		putcbar(low);
+	putchar('\n');
+
+	return (0);
+}
