81b8d3763ca1268d6fb6e41c6927ed9d52ba953e
Amypetua <anachusichiamaka30@gmail.com>
2022-12-12 17:21:02 -0800

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int d;
+
+	for (d = '0'; d <= '9';
+			putchar(d);
+	putchar('\n');
+
+	return (0);
+}
