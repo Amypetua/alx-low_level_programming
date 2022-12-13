48cd0f53b58df2f8cc907ea2923b3cd90d48e5ab
Amypetua <anachusichiamaka30@gmail.com>
2022-12-12 18:38:37 -0800

    Combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int d;
+
+	for (d = '0'; d <= '9'; d++)
+	{
+		putchar(d);
+		if (d != '9')
+		{
+			putchat(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
