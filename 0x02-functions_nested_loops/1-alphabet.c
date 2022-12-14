469cc062948fd1694f088034045db6b92fbefc19
Amypetua <anachusichiamaka30@gmail.com>
2022-12-13 22:00:12 -0800

    Aphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include <main.h>
+#include <unistd.h>
+
+/**
+ * print_alphabet - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+void print_alphabet(void)
+{
+	 char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+
+	}
+	 _putchar('\n');
+
+}
