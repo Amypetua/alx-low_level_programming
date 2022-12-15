3ebc094b442296ae8d324adf0adb28427af880ad
Amypetua <anachusichiamaka30@gmail.com>
2022-12-14 21:03:11 -0800

    Time table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,7 +1,8 @@
 #include "main.h"
+
 /**
- * time_table - checks description
- * Description: It prints 9 times table starting wih 0
+ * times_table - check description
+ * Description: It prints 9 times table starting with 0
  * Return: Nothing.
  */
 void times_table(void)
@@ -10,35 +11,31 @@ void times_table(void)
 
 	for (i = 0; i <= 9; i++)
 	{
-		for (j = 0; j = 9; j++)
-
+		for (j = 0; j <= 9; j++)
+		{
 			n = i * j;
 
-		if ((n / 10) == 0)
-		{
-			if (j != 0)
-				_putchar (' ');
-			_putchar(n + '0');
+			if ((n / 10) == 0)
+			{
+				if (j != 0)
+					_putchar(' ');
+				_putchar(n + '0');
 
-			_putchar(' ');
-
-			if (j == 9)
-				continue;
-			_putchar(',');
-			_putchar(' ');
+				if (j == 9)
+					continue;
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar((n / 10) + '0');
+				_putchar((n % 10) + '0');
+				if (j == 9)
+					continue;
+				_putchar(',');
+				_putchar(' ');
+			}
 		}
+		_putchar('\n');
 	}
-
-		else
-		{
-			_putchar((n / 10) + '0');
-			_putchar((n % 10) + '0');
-			if (j == 9)
-				continue;
-			_putchar(',');
-			_putchar(' ');
-		}
-	}
-	_putchar('\n');
-
 }
